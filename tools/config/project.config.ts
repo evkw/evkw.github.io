3874a334a8ac86387cdca9deb9c3c5fe0c0951f1
ewallace <ewallace@berkleyre.com.au>
2016-10-12 10:26:56 +1100

    add to home screen

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -20,8 +20,6 @@ export class ProjectConfig extends SeedConfig {
     // Add `NPM` third-party libraries to be injected/bundled.
     this.NPM_DEPENDENCIES = [
       ...this.NPM_DEPENDENCIES,
-       {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
-       {src: 'jquery-validation/dist/jquery.validate.js', inject: 'libs'},
       // {src: 'lodash/lodash.min.js', inject: 'libs'},
       {src: 'dexie/dist/dexie.min.js', inject: 'libs'},
     ];
